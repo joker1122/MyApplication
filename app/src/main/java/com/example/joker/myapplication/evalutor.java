b8a8878494ad72ee33df50895a88d43787ebc306
joker <18679677758@wo.cn>
2017-12-19 17:17:15 +0800

    message

--- app/src/main/java/com/example/joker/myapplication/evalutor.java
+++ app/src/main/java/com/example/joker/myapplication/evalutor.java
@@ -6,12 +6,13 @@ import android.animation.TypeEvaluator;
  * Created by joker on 2017/12/19.
  */
 
-public class evalutor implements TypeEvaluator {
+public class evalutor implements TypeEvaluator<Integer> {
     public evalutor() {
     }
     @Override
-    public Object evaluate(float fraction, Object startValue, Object endValue) {
-        return null;
+
+    public Integer evaluate(float fraction, Integer startValue, Integer endValue) {
+        return 0;
     }
 
 }
